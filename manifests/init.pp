b22f72ffe26dab6abe8c2bd43cf43c11db46a0c8
Matthew Stone <matt.stone@cdk.com>
2015-04-21 06:50:56 -0700

    updated init with better code

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,50 +1,39 @@
-# Class: ie_esc
-#
-# This module manages ie_esc
-#
-# Parameters: none
-#
-# Actions:
-#
-# Requires: see Modulefile
-#
-# Sample Usage:
-#
-define ie_esc(
+# Defined Type: ie_esc
+define ie_esc (
   $ensure = UNDEF,
   $users = UNDEF,
   $administrators = UNDEF,
 ){
-  
+  $key_loc = 'HKLM\SOFTWARE\Microsoft\Active Setup\Installed Components'
   case $ensure {
-    'present': { 
-      case $users {
-        UNDEF,'on': { $esc_user_data = 1 }
-        'off': { $esc_user_data = 0 }
-        default: { $esc_user_data = 1 }
+    'present' : {
+      $esc_user_data = $users ? {
+        'off'   => 0,
+        default => 1,
       }
-      case $administrators {
-        UNDEF,'on': { $esc_admin_data = 1 }
-        'off': { $esc_admin_data = 0 }
-        default: { $esc_admin_data = 1 }
-      }    
+      $esc_admin_data = $administrators ? {
+        'off'   => 0,
+        default => 1,
+      }
     }
-    'absent': { 
+    'absent'  : {
       $esc_admin_data = 0
       $esc_user_data = 0
     }
-    default: { fail('You must specify ensure status...') }  
+    default   : { fail('You must specify ensure status...') }
   }
-  registry::value{'IE ESC for Administrators':
-    key    => 'hklm\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}',
-    value  => 'IsInstalled',
-    type   => dword,
-    data   => $esc_admin_data,
+
+  registry::value { 'IE ESC for Administrators' :
+    key   => "${key_loc}\\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}",
+    value => 'IsInstalled',
+    type  => dword,
+    data  => $esc_admin_data,
   }
-    registry::value {'IE ESC for Users':
-    key    => 'hklm\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}',
-    value  => 'IsInstalled',
-    type   => dword,
-    data   => $esc_user_data,
+
+    registry::value { 'IE ESC for Users' :
+    key   => "${key_loc}\\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}",
+    value => 'IsInstalled',
+    type  => dword,
+    data  => $esc_user_data,
   }
 }
